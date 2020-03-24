9065eacd214ad5c9e2c594b49a7c54917dafd35d
Gavamot <gavamot@yandex.ru>
2020-03-24 14:21:23 +0500

    'change_namespace'

---
+++ MustDo/AttemptsOverException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MustDo
+{
+    public class AttemptsOverException : Exception
+    {
+        public AttemptsOverException(Exception e) : base("Attempts over", e)
+        {
+
+        }
+    }
+}
